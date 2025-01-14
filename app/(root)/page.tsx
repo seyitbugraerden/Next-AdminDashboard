d528c7db98dbaa90b443cd7b4464d45dac095323
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-14 21:55:12 +0300

    Book Cover Image & Config Fixes

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
+    </>
+  );
+};
+
+export default page;
